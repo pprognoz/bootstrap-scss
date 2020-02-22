65d6fc329264d436f9ea6eda1eeefb2277905be7
useHTML5 <mne@sait-nado.ru>
2020-02-22 13:28:18 +0500

    init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bootstrap-scss",
-    version="4.4.1/0",
+    version="4.4.1-1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
